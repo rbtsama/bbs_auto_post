5074e4dc748250a74d0274be8562f603e9da3beb
rbtsama <rbtsama@gmail.com>
2025-04-04 03:33:39 +0800

    全面修复Vercel部署问题：简化配置并优化路由设置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/Home.vue';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),
   routes
 });
 
